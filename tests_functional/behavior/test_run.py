d22298dfc67c2adfbbd455c5d8e8ffb08acdf817
Tao Sauvage <sauvage.tao@gmail.com>
2014-11-07 22:10:48 +0100

    Proper check that OWTF has successfully run.

--- tests_functional/behavior/test_run.py
+++ tests_functional/behavior/test_run.py
@@ -10,18 +10,16 @@ import owtf
 class OWTFRunTest(unittest.TestCase):
     @mock.patch('__builtin__.raw_input', return_value=['Y'])
     @mock.patch('framework.interface.server.InterfaceServer.start', side_effect=KeyboardInterrupt)
-    @mock.patch('owtf.logging.warn')
     @mock.patch('owtf.logging.info')
-    def test_run_no_param(self, log_info, log_warn, server_start, rvalue):
+    @mock.patch('owtf.logging.warn')
+    def test_run_no_param(self, log_warn, log_info, server_start, rvalue):
         """Run OWTF without any parameter."""
         sys.argv = ['owtf.py', ]
-        try:
+        with self.assertRaises(SystemExit) as cm:
             owtf.main(sys.argv)
-        except SystemExit:  # Expected behavior (raised by exit())
-            pass
-        self.assertTrue(
-            'Web UI URL' in str(call)
-            for call in log_warn.call_args_list)
-        self.assertTrue(
-            'Press Ctrl+C' in str(call)
-            for call in log_info.call_args_list)
+        # Check that OWTF exited properly exit(0).
+        self.assertEqual(cm.exception.code, 0, 'OWTF did not exit properly')
+        # Check that Web UI has started.
+        assert_that(str(log_warn.call_args_list), contains_string('Web UI URL'))
+        # Check that Web UI has properly started.
+        assert_that(str(log_info.call_args_list), contains_string('Press Ctrl+C'))
