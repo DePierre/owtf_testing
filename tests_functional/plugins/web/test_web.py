f558afef43cfe5258896c5dfa067fd7e9ac4831d
Tao Sauvage <sauvage.tao@gmail.com>
2014-11-30 16:02:58 +0100

    [plugins] Add tests for each category.

--- tests_functional/plugins/web/test_web.py
+++ tests_functional/plugins/web/test_web.py
@@ -13,3 +13,31 @@ class OWTFWebPluginTest(OWTFWebPluginTestCase):
             'owtf.py', '-g', 'web', '-t', 'active', "%s://%s:%s" %
             (self.PROTOCOL, self.IP, self.PORT)]
         owtf.main(args)
+
+    def test_web_passive(self):
+        """Test OWTF WEB passive plugins."""
+        args = [
+            'owtf.py', '-g', 'web', '-t', 'passive', "%s://%s:%s" %
+            (self.PROTOCOL, self.IP, self.PORT)]
+        owtf.main(args)
+
+    def test_web_semi_passive(self):
+        """Test OWTF WEB semi-passive plugins."""
+        args = [
+            'owtf.py', '-g', 'web', '-t', 'semi_passive', "%s://%s:%s" %
+            (self.PROTOCOL, self.IP, self.PORT)]
+        owtf.main(args)
+
+    def test_web_external(self):
+        """Test OWTF WEB external plugins."""
+        args = [
+            'owtf.py', '-g', 'web', '-t', 'external', "%s://%s:%s" %
+            (self.PROTOCOL, self.IP, self.PORT)]
+        owtf.main(args)
+
+    def test_web_grep(self):
+        """Test OWTF WEB grep plugins."""
+        args = [
+            'owtf.py', '-g', 'web', '-t', 'grep', "%s://%s:%s" %
+            (self.PROTOCOL, self.IP, self.PORT)]
+        owtf.main(args)
