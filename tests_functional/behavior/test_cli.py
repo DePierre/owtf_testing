45d36936f13a6ebe77005b3051d001e8cf9bc8a5
Tao Sauvage <sauvage.tao@gmail.com>
2014-11-30 15:46:38 +0100

    [cli] Split listing tests into a test each (web, net, aux).

--- tests_functional/behavior/test_cli.py
+++ tests_functional/behavior/test_cli.py
@@ -1,5 +1,3 @@
-import sys
-
 import mock
 from hamcrest import *
 
@@ -8,17 +6,27 @@ import owtf
 
 
 class OWTFCliTest(OWTFTestCase):
-    @mock.patch('framework.interface.server.FileServer')
-    @mock.patch('framework.plugin.worker_manager.Worker')
-    def test_run_list_plugins(self, mock_worker, mock_fileserver):
-        """Run OWTF to list the plugins."""
-        for cat in ('web', 'net', 'aux'):
-            sys.argv = ['owtf.py', '-l', cat]
-            # Force empty tasks for workers.
-            mock_worker.input_q.get.return_value = ()
-            with self.assertRaises(SystemExit) as cm:
-                owtf.main(sys.argv)
-            # Check that OWTF exited properly exit(0).
-            self.assertEqual(cm.exception.code, 0, 'OWTF did not exit properly')
-            # Check that Web UI has properly started.
-            assert_that(str(self.mock_log_info.call_args_list), contains_string('Press Ctrl+C'))
+
+    def test_cli_list_plugins_aux(self):
+        """Run OWTF to list the aux plugins."""
+        args = ['owtf.py', '-l', 'aux']
+        owtf.main(args)
+        assert_that(
+            str(self.mock_log_info.call_args_list),
+            contains_string("Available AUXILIARY plugins"))
+
+    def test_cli_list_plugins_net(self):
+        """Run OWTF to list the net plugins."""
+        args = ['owtf.py', '-l', 'net']
+        owtf.main(args)
+        assert_that(
+            str(self.mock_log_info.call_args_list),
+            contains_string("Available NET plugins"))
+
+    def test_cli_list_plugins_web(self):
+        """Run OWTF to list the web plugins."""
+        args = ['owtf.py', '-l', 'web']
+        owtf.main(args)
+        assert_that(
+            str(self.mock_log_info.call_args_list),
+            contains_string("Available WEB plugins"))
