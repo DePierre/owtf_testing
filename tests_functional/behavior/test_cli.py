abe08158e569ee36fd37bcaa581fd38eb048928e
Tao Sauvage <sauvage.tao@gmail.com>
2014-12-06 19:30:14 +0100

    [cli] Complete tests for listing plugins.

--- tests_functional/behavior/test_cli.py
+++ tests_functional/behavior/test_cli.py
@@ -7,26 +7,80 @@ import owtf
 
 class OWTFCliTest(OWTFTestCase):
 
-    def test_cli_list_plugins_aux(self):
+    @mock.patch('framework.plugin.plugin_handler.PluginHandler.ShowPluginTypePlugins')
+    def test_cli_list_plugins_aux(self, mock_show_plugin_type_plugins):
         """Run OWTF to list the aux plugins."""
         args = ['owtf.py', '-l', 'aux']
         owtf.main(args)
+        # Check plugin group.
         assert_that(
-            str(self.mock_log_info.call_args_list),
-            contains_string("Available AUXILIARY plugins"))
+            str(mock_show_plugin_type_plugins.call_args_list),
+            contains_string("'aux'"))
+        # Check plugin types.
+        assert_that(
+            str(mock_show_plugin_type_plugins.call_args_list),
+            contains_string("'exploit'"))
+        assert_that(
+            str(mock_show_plugin_type_plugins.call_args_list),
+            contains_string("'smb'"))
+        assert_that(
+            str(mock_show_plugin_type_plugins.call_args_list),
+            contains_string("'bruteforce'"))
+        assert_that(
+            str(mock_show_plugin_type_plugins.call_args_list),
+            contains_string("'dos'"))
+        assert_that(
+            str(mock_show_plugin_type_plugins.call_args_list),
+            contains_string("'wafbypasser'"))
+        assert_that(
+            str(mock_show_plugin_type_plugins.call_args_list),
+            contains_string("'se'"))
+        assert_that(
+            str(mock_show_plugin_type_plugins.call_args_list),
+            contains_string("'rce'"))
+        assert_that(
+            str(mock_show_plugin_type_plugins.call_args_list),
+            contains_string("'selenium'"))
 
-    def test_cli_list_plugins_net(self):
+    @mock.patch('framework.plugin.plugin_handler.PluginHandler.ShowPluginTypePlugins')
+    def test_cli_list_plugins_net(self, mock_show_plugin_type_plugins):
         """Run OWTF to list the net plugins."""
         args = ['owtf.py', '-l', 'net']
         owtf.main(args)
+        # Check plugin group.
         assert_that(
-            str(self.mock_log_info.call_args_list),
-            contains_string("Available NET plugins"))
+            str(mock_show_plugin_type_plugins.call_args_list),
+            contains_string("'net'"))
+        # Check plugin types.
+        assert_that(
+            str(mock_show_plugin_type_plugins.call_args_list),
+            contains_string("'active'"))
+        assert_that(
+            str(mock_show_plugin_type_plugins.call_args_list),
+            contains_string("'bruteforce'"))
 
-    def test_cli_list_plugins_web(self):
+    @mock.patch('framework.plugin.plugin_handler.PluginHandler.ShowPluginTypePlugins')
+    def test_cli_list_plugins_web(self, mock_show_plugin_type_plugins):
         """Run OWTF to list the web plugins."""
         args = ['owtf.py', '-l', 'web']
         owtf.main(args)
+        # Check plugin group.
         assert_that(
-            str(self.mock_log_info.call_args_list),
-            contains_string("Available WEB plugins"))
+            str(mock_show_plugin_type_plugins.call_args_list),
+            contains_string("'web'"))
+        # Check plugin types.
+        assert_that(
+            str(mock_show_plugin_type_plugins.call_args_list),
+            contains_string("'external'"))
+        assert_that(
+            str(mock_show_plugin_type_plugins.call_args_list),
+            contains_string("'active'"))
+        assert_that(
+            str(mock_show_plugin_type_plugins.call_args_list),
+            contains_string("'passive'"))
+        assert_that(
+            str(mock_show_plugin_type_plugins.call_args_list),
+            contains_string("'grep'"))
+        assert_that(
+            str(mock_show_plugin_type_plugins.call_args_list),
+            contains_string("'semi_passive'"))
