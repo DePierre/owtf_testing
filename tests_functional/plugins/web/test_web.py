41a4b1fac073d8972e5a1021f9b63293ab10d76f
Tao Sauvage <sauvage.tao@gmail.com>
2014-12-10 02:45:16 +0100

    Update status of the web test case.

--- tests_functional/plugins/web/test_web.py
+++ tests_functional/plugins/web/test_web.py
@@ -1,43 +1,44 @@
+import unittest
 import mock
 from hamcrest import *
 
-from owtf_testing.utils.owtf import OWTFWebPluginTestCase
-import owtf
+from owtf_testing.utils.owtftest import OWTFCliWebPluginTestCase
 
 
-class OWTFWebPluginTest(OWTFWebPluginTestCase):
+@unittest.skip("Currently broken.")
+class OWTFCliWebPluginTest(OWTFCliWebPluginTestCase):
 
     def test_web_active(self):
         """Test OWTF WEB active plugins."""
         args = [
             'owtf.py', '-g', 'web', '-t', 'active', "%s://%s:%s" %
             (self.PROTOCOL, self.IP, self.PORT)]
-        owtf.main(args)
+        self.run_owtf()
 
     def test_web_passive(self):
         """Test OWTF WEB passive plugins."""
         args = [
             'owtf.py', '-g', 'web', '-t', 'passive', "%s://%s:%s" %
             (self.PROTOCOL, self.IP, self.PORT)]
-        owtf.main(args)
+        self.run_owtf()
 
     def test_web_semi_passive(self):
         """Test OWTF WEB semi-passive plugins."""
         args = [
             'owtf.py', '-g', 'web', '-t', 'semi_passive', "%s://%s:%s" %
             (self.PROTOCOL, self.IP, self.PORT)]
-        owtf.main(args)
+        self.run_owtf()
 
     def test_web_external(self):
         """Test OWTF WEB external plugins."""
         args = [
             'owtf.py', '-g', 'web', '-t', 'external', "%s://%s:%s" %
             (self.PROTOCOL, self.IP, self.PORT)]
-        owtf.main(args)
+        self.run_owtf()
 
     def test_web_grep(self):
         """Test OWTF WEB grep plugins."""
         args = [
             'owtf.py', '-g', 'web', '-t', 'grep', "%s://%s:%s" %
             (self.PROTOCOL, self.IP, self.PORT)]
-        owtf.main(args)
+        self.run_owtf()
