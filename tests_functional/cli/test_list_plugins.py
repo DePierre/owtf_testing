78f44ee32a1df99f1b8be9719834947d352dda18
Tao Sauvage <sauvage.tao@gmail.com>
2014-12-10 02:43:56 +0100

    Test long name options as well.

--- tests_functional/cli/test_list_plugins.py
+++ tests_functional/cli/test_list_plugins.py
@@ -15,26 +15,29 @@ class OWTFCliListPluginsTest(OWTFCliTestCase):
     @log_capture()
     def test_cli_list_plugins_aux(self, logger):
         """Run OWTF to list the aux plugins."""
-        self.args += ['-l', 'aux']
         expected = ['Available AUXILIARY plugins', 'exploit', 'smb', 'bruteforce', 'dos', 'wafbypasser', 'se', 'rce', 'selenium']
-        self.run_owtf()
 
+        self.run_owtf('-l', 'aux')
+        self.assert_are_in_logs(logger, expected)
+        self.run_owtf('--list_plugins', 'aux')
         self.assert_are_in_logs(logger, expected)
 
     @log_capture()
     def test_cli_list_plugins_net(self, logger):
         """Run OWTF to list the net plugins."""
-        self.args += ['-l', 'net']
         expected = ['Available NET plugins', 'active', 'bruteforce']
-        self.run_owtf()
 
+        self.run_owtf('-l', 'net')
+        self.assert_are_in_logs(logger, expected)
+        self.run_owtf('--list_plugins', 'net')
         self.assert_are_in_logs(logger, expected)
 
     @log_capture()
     def test_cli_list_plugins_web(self, logger):
         """Run OWTF to list the web plugins."""
-        self.args += ['-l', 'web']
         expected = ['Available WEB plugins', 'external', 'active', 'passive', 'grep', 'semi_passive']
-        self.run_owtf()
 
+        self.run_owtf('-l', 'web')
+        self.assert_are_in_logs(logger, expected)
+        self.run_owtf('--list_plugins', 'web')
         self.assert_are_in_logs(logger, expected)
