8e8b6e56baf9bdd17b30136f1666297390863d98
Tao Sauvage <sauvage.tao@gmail.com>
2014-12-10 02:44:24 +0100

    Move nowebui test to another file.

--- tests_functional/cli/test_empty_run.py
+++ tests_functional/cli/test_empty_run.py
@@ -12,27 +12,7 @@ from framework.core import Core
 class OWTFCliEmptyRunTest(OWTFCliTestCase):
 
     @log_capture()
-    def test_cli_no_webui(self, logger):
-        """Run OWTF without its Web UI."""
+    def test_cli_empty_run(self, logger):
+        """Run OWTF without parameters."""
         self.run_owtf()
         self.assert_has_been_logged(logger, "All jobs have been done. Exiting.")
-
-    @log_capture()
-    def test_cli_with_target(self, logger):
-        """Run OWTF with a target."""
-        self.args += ['%s://%s:%s' % (self.PROTOCOL, self.IP, self.PORT)]
-        self.run_owtf()
-        self.assert_has_been_logged(
-            logger,
-            "The IP address for %s is: '%s'" % (self.IP, self.IP))
-
-    @log_capture()
-    def test_cli_with_target_twice(self, logger):
-        """Run OWTF with a target twice."""
-        # Add the target twice and check that OWTF use the database to retrieve
-        # the IP address of the target when checking the second one.
-        self.args += ['%s://%s:%s' % (self.PROTOCOL, self.IP, self.PORT)]
-        self.run_owtf()
-        self.assert_has_been_logged(
-            logger,
-            "%s://%s:%s already exists in DB" % (self.PROTOCOL, self.IP, self.PORT))
---
+++ tests_functional/cli/test_nowebui.py
@@ -0,0 +1,31 @@
+import mock
+from hamcrest import *
+
+from testfixtures import log_capture
+
+from owtf_testing.utils.utils import dummy
+from owtf_testing.utils.owtftest import OWTFCliTestCase
+
+from framework.core import Core
+
+
+@mock.patch.object(Core, 'enable_logging', dummy)
+class OWTFCliNoWebUITest(OWTFCliTestCase):
+
+    @log_capture()
+    def test_cli_no_webui(self, logger):
+        """Run OWTF without its Web UI."""
+        self.run_owtf()
+        self.assert_has_not_been_logged(
+            logger,
+            "http://127.0.0.1:8009 <-- Web UI URL")
+        self.assert_has_been_logged(logger, "All jobs have been done. Exiting.")
+
+    @log_capture()
+    def test_cli_no_webui_again(self, logger):
+        """Run OWTF without its Web UI again to check if resources are freed."""
+        self.run_owtf()
+        self.assert_has_not_been_logged(
+            logger,
+            "http://127.0.0.1:8009 <-- Web UI URL")
+        self.assert_has_been_logged(logger, "All jobs have been done. Exiting.")
