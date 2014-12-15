6d5585d287a3790fff29fac0f949dc8c0293544e
Tao Sauvage <sauvage.tao@gmail.com>
2014-12-15 21:44:42 +0100

    Test for OWTF-WVS-006.

---
+++ tests_functional/plugins/web/active/test_owcs_wvs_006.py
@@ -0,0 +1,23 @@
+import mock
+from hamcrest import *
+
+from owtf_testing.utils.owtftest import OWTFCliWebPluginTestCase
+
+
+class OWTFCliWebActiveWVS006PluginTest(OWTFCliWebPluginTestCase):
+
+    categories = ['plugins', 'web', 'owtf-wvs-006']
+
+    def test_web_active(self):
+        """Test OWTF WEB active plugins."""
+        self.run_owtf('-o', 'OWTF-WVS-006', "%s://%s:%s" % (self.PROTOCOL, self.IP, self.PORT))
+        # Test Skipfish went OK.
+        self.assert_is_in_logs(
+            '1 - Target: http://127.0.0.1:8888 -> Plugin: Skipfish Unauthenticated (active)',
+            name='Worker')
+        self.assert_is_in_logs('This was a great day for science!', name='Worker')
+        self.assert_is_in_logs('Execution Start Date/Time:', name='Worker')
+        # Test no other plugin has been run.
+        self.assert_is_not_in_logs('2 - Target:', name='Worker')
+        # Test OWTF exited cleanly.
+        self.assert_is_in_logs('All jobs have been done. Exiting.', name='MainProcess')
