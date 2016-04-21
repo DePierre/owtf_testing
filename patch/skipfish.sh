9b92b1ee06cd1f49d9ba45d6541197b00efabd65
Tao Sauvage <sauvage.tao@gmail.com>
2016-04-21 15:59:03 +0200

    [patch] Manually move skipfish binaries.

--- patch/skipfish.sh
+++ patch/skipfish.sh
@@ -4,4 +4,9 @@ tar zxvf skipfish-2.10b.tgz
 sudo apt-get install libidn11-dev
 cd skipfish-2.10b
 make
-sudo make install
+sudo rm -rf /usr/share/skipfish/*
+sudo cp -rf ./assets /usr/share/skipfish/
+sudo cp -rf ./dictionaries /usr/share/skipfish/
+sudo cp -rf ./signatures /usr/share/skipfish/
+sudo rm /usr/bin/skipfish
+sudo cp -f ./skipfish /usr/bin/
